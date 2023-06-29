c512670e543e684c218985a3e8647adb6ada8edc
dikapt <120253697+dikapt@users.noreply.github.com>
2023-06-29 23:53:08 +0300

    validation.rb

--- validation.rb
+++ validation.rb
@@ -20,11 +20,21 @@ def self.after_date?(date)
   date < Date.today
 end
 end
-puts Validation.valid_name?("Maria-Anna")
-puts Validation.valid_name?("Serjio2")
+puts Validation.valid_name?("Anna")
+puts Validation.valid_name?("Anna-Maria")
+puts Validation.valid_name?("Каріна-Ірина")
+
+puts Validation.valid_name?("An-")
+puts Validation.valid_name?("-")
+puts Validation.valid_name?("aN")
+puts Validation.valid_name?("AnnA")
 
 puts Validation.valid_inn?("AZ1234543456")
-puts Validation.valid_inn?("AC12345678901")
+
+puts Validation.valid_inn?("AЯ6787787654")
+puts Validation.valid_inn?("az1234567898")
+puts Validation.valid_inn?("5676567656AZ")
+puts Validation.valid_inn?("AZ78987")
 
 puts Validation.after_date?(Date.new(2003, 9, 30))
 puts Validation.after_date?(Date.today)
